6559371003094838c1153587410074ac5a1e4edb
Matthew Jigalin <mattjigs@gmail.com>
2021-11-22 14:18:11 +1100

    Broken Build

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -6,30 +6,59 @@ import logo from "./components/PokemonLogo.png";
 function App() {
   const [cachedPokeBoys, setCachedPokeBoys] = useState([]);
   const [shownPokeBoys, setShownPokeBoys] = useState([]);
-  //   const [searchInput, setSearchInput] = useState("");
+  const [pageRef, setPageRef] = useState(0);
 
+  console.log(cachedPokeBoys);
+  // Fetch some data with a given page reference (10 size for debugging)
+  // Load response to .json format
+  // Set CACHED pokeboys to the results
+  // Set WORKING COPY / SHOWN pokeboys to what the cached copy currently is
   const pokeData = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100")
+    console.log("PokeData Called");
+    fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${pageRef * 10}&limit=10`)
       .then((response) => response.json())
       .then((data) => setCachedPokeBoys(data.results));
     setShownPokeBoys(cachedPokeBoys);
   };
 
+  // On first load - log something
+  // AND call pokeData function.
   useEffect(() => {
+    console.log("UseEffect Triggered");
     pokeData();
   }, []);
 
+  // What happens as we type?
+  // Convert to lowercase
+  // Filter our STORED COPY of pokeboys
+  // Filter based off NAME data
+  // Set our WORKING COPY of pokeboys to the result
+  // OTHERWISE - if we filter the cached copy, then as you delete your search
   const onSearchbarChange = (e) => {
-    //   setSearchInput(e.target.value);
-    // setStoredPokeBoys(storedPokeBoys);
-
     let value = e.target.value.toLowerCase();
     let result = [];
-    console.log(value);
     result = cachedPokeBoys.filter((data) => {
-      return data.name.search(value) != -1;
+      return data.name.search(value) !== -1;
     });
     setShownPokeBoys(result);
+  };
+
+  // Click BACK arrow to update the referenced page we're on
+  const backBtn = (e) => {
+    console.log("Current Page:" + pageRef);
+    if (pageRef > 0) {
+      setPageRef(pageRef - 1);
+      pokeData();
+    }
+  };
+
+  // Click NEXT arrow to update the referenced page we're on
+  const nextBtn = (e) => {
+    console.log("Current Page:" + pageRef);
+    if (pageRef < 10) {
+      setPageRef(pageRef + 1);
+      pokeData();
+    }
   };
 
   return (
@@ -37,7 +66,7 @@ function App() {
       <div className="headerWrapper">
         <img src={logo} alt="Logo" className="header-logo" />
         <form className="search-pokemon">
-          <label for="pokename">Pokemon Search:</label>
+          <label htmlFor="pokename">Page Search:</label>
           <input
             type="text"
             id="pokename"
@@ -46,6 +75,15 @@ function App() {
           ></input>
         </form>
       </div>
+      <div className="homepage-nav">
+        <button onClick={backBtn} className="nav-button-back">
+          &lt;&lt;
+        </button>
+        <button onClick={nextBtn} className="nav-button-forward">
+          &gt;&gt;
+        </button>
+      </div>
+      <h1>{pageRef}</h1>
       <div className="pokemon-container">
         {shownPokeBoys.map((individualPokemon) => {
           return (
@@ -55,6 +93,17 @@ function App() {
             />
           );
         })}
+      </div>
+      <div className="homepage-nav">
+        <button onClick={backBtn} className="nav-button-back">
+          &lt;&lt;
+        </button>
+        <button onClick={nextBtn} className="nav-button-forward">
+          &gt;&gt;
+        </button>
+      </div>
+      <div className="homepage-footer">
+        <img src={logo} alt="Logo" className="footer-logo" />
       </div>
     </div>
   );
